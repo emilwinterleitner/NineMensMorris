577977af542e3eb2038b1451d57cdb15b806dce6
Emil Winterleitner <emil.winterleitner@student.tugraz.at>
2021-11-08 23:00:45 +0100

    Structured dir, added background

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("View/RootWindow.fxml"));
+        primaryStage.setTitle("Nine Men's Morris");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        String OS;
+
+        OS = System.getProperty("os.name").toLowerCase();
+
+        // worked in swing in the past - maybe we dont need it afterall
+        if (OS.contains("mac")) {
+            System.setProperty("com.apple.mrj.application.apple.menu.about.name", "Nine Men's Morris"); // does not seem to work in IDE
+            System.setProperty("apple.laf.useScreenMenuBar", "true");
+        }
+
+        launch(args);
+    }
+}

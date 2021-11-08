6779a7f5ff69a3c4bb140ad1bd5b1fa10ee54eff
Emil Winterleitner <emil.winterleitner@student.tugraz.at>
2021-11-08 23:31:20 +0100

    Added menu stub

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -1,4 +1,17 @@
 package Controller;
 
+import javafx.event.ActionEvent;
+
 public class Controller {
+    public void handleNewGame(ActionEvent actionEvent) {
+        System.out.println("New Game");
+    }
+
+    public void handleSave(ActionEvent actionEvent) {
+        System.out.println("Save");
+    }
+
+    public void handleLoad(ActionEvent actionEvent) {
+        System.out.println("Load");
+    }
 }
